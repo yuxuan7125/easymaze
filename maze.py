ff39e7dda3ee2a9c42d61b0853c0b8626b359354
YuXuan <empoweryx@gmail.com>
2026-01-17 14:46:33 +0800

    Update maze.py

--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 R=int(input())      #迷宮大小
 C=int(input())
@@ -18,6 +19,8 @@ for i in range(R):      #生成初始迷宮
                         p[i][j-1]=1
                     case 4 :
                         p[i-1][j-1]=1
+for row in p :
+    print(*row)
 rs=random.randrange(0,R)        #生成開始點s及結束點e
 cs=random.randrange(0,C)
 re=random.randrange(0,R)
@@ -30,45 +33,45 @@ while abs(rs-re)+abs(cs-ce)<(R+C)/2 :
     if k>100:
         break
 p[rs][cs]=p[re][ce]=0
+def count(a,b,item,maze):        #找出item方位
+    lis=list()
+    if a-1>=0 and maze[a-1][b] in item:
+        lis.append(1)
+    if b-1>=0 and maze[a][b-1] in item:
+        lis.append(2)
+    if a+1<R and maze[a+1][b] in item:
+        lis.append(3)
+    if b+1<C and maze[a][b+1] in item:
+        lis.append(4)
+    return lis
+def countbomb(a,b,maze):        #找出炸開路徑方位
+    lis=list()
+    if a-1>=0 and p[a-1][b]==1 and len(count(a-1,b,['v'],maze))==1:
+        lis.append(1)
+    if b-1>=0 and p[a][b-1]==1 and len(count(a,b-1,['v'],maze))==1:
+        lis.append(2)
+    if a+1<R and p[a+1][b]==1 and len(count(a+1,b,['v'],maze))==1:
+        lis.append(3)
+    if b+1<C and p[a][b+1]==1 and len(count(a,b+1,['v'],maze))==1:
+        lis.append(4)
+    return lis
 r,c=rs,cs
-def count(a,b,item):        #找出item方位
-    lis=list()
-    if a-1>=0 and p[a-1][b]==item:
-        lis.append(1)
-    if b-1>=0 and p[a][b-1]==item:
-        lis.append(2)
-    if a+1<R and p[a+1][b]==item:
-        lis.append(3)
-    if b+1<C and p[a][b+1]==item:
-        lis.append(4)
-    return lis
-def countbomb(a,b):        #找出炸開路徑方位
-    lis=list()
-    if a-1>=0 and p[a-1][b]==1 and len(count(a-1,b,2))==1:
-        lis.append(1)
-    if b-1>=0 and p[a][b-1]==1 and len(count(a,b-1,2))==1:
-        lis.append(2)
-    if a+1<R and p[a+1][b]==1 and len(count(a+1,b,2))==1:
-        lis.append(3)
-    if b+1<C and p[a][b+1]==1 and len(count(a,b+1,2))==1:
-        lis.append(4)
-    return lis
 way=[[r,c]]
 i=back=0
-def add_d():
+def add_d(maze):
     if len(way[i])==2:
-        way[i].append(count(way[i][0],way[i][1],0))
+        way[i].append(count(way[i][0],way[i][1],[0],maze))
 while r!=re or c!=ce :
-    p[r][c]=2
-    add_d()
+    p[r][c]='v'
+    add_d(p)
     if len(way[i][2])==0:
         while len(way[i][2])==0:
             if i==back:         #開路
                 bomb=random.randrange(0,len(way))
-                while len(countbomb(way[bomb][0],way[bomb][1]))==0:
+                while len(countbomb(way[bomb][0],way[bomb][1],p))==0:
                     del way[bomb]
                     bomb=random.randrange(0,len(way))
-                match random.choice(countbomb(way[bomb][0],way[bomb][1])):
+                match random.choice(countbomb(way[bomb][0],way[bomb][1],p)):
                     case 1:
                         r,c=way[bomb][0]-1,way[bomb][1]
                     case 2:
@@ -81,7 +84,7 @@ while r!=re or c!=ce :
                 way.append([r,c])
                 i=len(way)-1
                 back=i
-                add_d()
+                add_d(p)
                 break
             i-=1
             r,c=way[i][0],way[i][1]
@@ -101,9 +104,41 @@ while r!=re or c!=ce :
     way.insert(i,[r,c])
 p[rs][cs]='s'
 p[re][ce]='e'
+print("maze:")
 for i in range(R):
     for j in range(C):
-        if p[i][j]==2:
+        if p[i][j]=='v':
             p[i][j]=0
-for row in p :
+    print(*p[i])
+answer=copy.deepcopy(p)
+answer[rs][cs]=answer[re][ce]=0
+r,c=rs,cs
+way=[[r,c]]
+i=0
+while r!=re or c!=ce :
+    answer[r][c]='v'
+    add_d(answer)
+    if len(way[i][2])==0:
+        while len(way[i][2])==0:
+            answer[r][c]=0
+            i-=1
+            r,c=way[i][0],way[i][1]
+        continue
+    d=random.choice(way[i][2])
+    way[i][2].remove(d)
+    match d:
+        case 1:
+            r-=1
+        case 2:
+            c-=1
+        case 3:
+            r+=1
+        case 4:
+            c+=1
+    i+=1
+    way.insert(i,[r,c])
+answer[rs][cs]='s'
+answer[re][ce]='e'
+print("answer:")
+for row in answer :
     print(*row)
